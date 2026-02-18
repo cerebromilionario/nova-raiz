5182db74324e9460cc238013ec8c9e173a60250b
cerebromilionario <will-dc@hotmail.com>
2026-02-18 16:46:38 -0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://novaraiz.netlify.app',
-  integrations: [
-    sitemap()
-  ]
-});
